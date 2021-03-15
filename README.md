073881da58b318933a463c0b77c4895c04d045ce
Tarjei Skrede <tarjeiskrede@M-C02Y7599JGH6.local>
2021-03-15 11:27:06 +0100

    Added check if data returned from service contains an error message instead of raising it as a HTTP exception

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# URL Post Rest Service
+# URL Post rest service
+
+A simple service that translates get requests to post requests.
+
+
+Example system config:
+----------------------
+
+```json
+{
+  "_id": "url-post-rest-system",
+  "type": "system:microservice",
+  "docker": {
+    "environment": {
+      "apiKey": "my-api-key",
+      "baseurl": "https://my.base.url.no/",
+      "page_number": "1",
+      "page_size": "1000",
+      "protocol": "json"
+    },
+    "image": "sesamcommunity/url-post-rest-service:latest",
+    "port": 5001
+  }
+}
+
+```
+
+Example pipe using the microservice above
+----------------------------------------------
+
+```json
+{
+  "_id": "url-post-rest-pipe",
+  "type": "pipe",
+  "source": {
+    "is_chronological": false,
+    "is_since_comparable": true,
+    "supports_since": false,
+    "system": "url-post-rest-system",
+    "type": "json",
+    "url": "my-url-post-rest-api/api"
+  }
+}
+
+```
--- service/url_post_rest_service.py
+++ service/url_post_rest_service.py
@@ -9,6 +9,9 @@ app = Flask(__name__)
 logger = logger.Logger('url-post-rest-service')
 url = os.environ.get("baseurl")
 apiKey = os.environ.get("apiKey")
+error_is_embedded = os.environ.get("error_is_embedded")
+embedded_error_message_property = os.environ.get("embedded_error_message_property")
+embedded_error_code_property = os.environ.get("embedded_error_code_property")
 
 
 @app.route("/<path:path>", methods=["GET"])
@@ -21,6 +24,16 @@ def get(path):
 
     try:
         response = requests.post(requesturl, data=json.dumps(data), headers=headers)
+
+        #Error messages may be sent as data with http status code 200 and not as exceptions (http status code 500 for example).
+        #Assumes that if the length of the response is greater than 300 characters, real data is returned.
+        if response.status_code == 200 and error_is_embedded and len(response.text) < 300:
+            response_data = json.loads(response.text)
+            if len(response_data) == 1:
+                error_entity = response_data[0]
+                if error_entity[embedded_error_code_property] != 200:
+                    raise Exception(error_entity[embedded_error_message_property])
+
     except Exception as e:
         logger.warn("Exception occured when download data from '%s': '%s'", requesturl, e)
         raise
